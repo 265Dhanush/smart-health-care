d8b230e29fd0c75873ba67eab0177bd5ce963116
subabhilash <163818572+subabhilash@users.noreply.github.com>
2024-04-09 07:06:51 +0000

    date time

--- app.py
+++ app.py
@@ -15,14 +15,16 @@ def hello_world():
 def submit_form():
     name = request.form["name"]
     age = request.form["age"]
+    date = request.form["date"]
+    time = request.form["time"]
     room_temperature = request.form["room_temperature"]
     room_humidity = request.form["room_humidity"]
-    heart_rate = request.form["heart_rate"]
+    pulse_rate = request.form["pulse_rate"]
     body_temperature = request.form["body_temperature"]
 
     cursor = mydb.cursor()
-    sql = "INSERT INTO patients (p_name,p_age,r_temp,r_humi,heart_rate,b_temp) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = (name, age, room_temperature, room_humidity, heart_rate, body_temperature)
+    sql = "INSERT INTO patients (p_date,p_time,p_name,age,pulse_rate,body_temperature,room_temperature,room_humidity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (date, time,name, age, pulse_rate, body_temperature, room_temperature, room_humidity)
     cursor.execute(sql, val)
     mydb.commit()
 
